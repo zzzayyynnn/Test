b2b5f90757701985e2935b5859772b6d0c8fb4e7
zzzayyynnn <isislamanda@gmail.com>
2026-01-25 11:21:19 +0800

    Update saveConfig.js

--- saveConfig.js
+++ saveConfig.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
 const path = require("path");
+
+// Path sa guild config JSON
 const CONFIG_FILE = path.join(__dirname, "../data/guildConfig.json");
 
+// Load config, kung wala file, return empty object
 function loadConfig() {
   if (!fs.existsSync(CONFIG_FILE)) return {};
-  return JSON.parse(fs.readFileSync(CONFIG_FILE));
+  return JSON.parse(fs.readFileSync(CONFIG_FILE, "utf8"));
 }
 
+// Save config sa JSON
 function saveConfig(config) {
   fs.writeFileSync(CONFIG_FILE, JSON.stringify(config, null, 2));
 }
