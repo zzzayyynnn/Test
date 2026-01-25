7f757f63802a8add8176e67e2300425c55a0b6a3
zzzayyynnn <isislamanda@gmail.com>
2026-01-25 11:16:37 +0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 
 const commands = [];
 const commandFiles = fs.readdirSync("./commands").filter(f => f.endsWith(".js"));
-
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   commands.push(command.data.toJSON());
@@ -15,10 +14,7 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 (async () => {
   try {
     console.log("Registering slash commands...");
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: commands }
-    );
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
     console.log("✅ Commands registered");
   } catch (err) {
     console.error(err);
