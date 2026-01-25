c4d57ce68a6b069327032ce2e4fb5d6ff2c7161e
zzzayyynnn <isislamanda@gmail.com>
2026-01-25 10:53:20 +0800

    Create saveConfig.js

---
+++ utils/saveConfig.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+const CONFIG_FILE = path.join(__dirname, "../data/guildConfig.json");
+
+function loadConfig() {
+  if (!fs.existsSync(CONFIG_FILE)) return {};
+  return JSON.parse(fs.readFileSync(CONFIG_FILE));
+}
+
+function saveConfig(config) {
+  fs.writeFileSync(CONFIG_FILE, JSON.stringify(config, null, 2));
+}
+
+module.exports = { loadConfig, saveConfig };
