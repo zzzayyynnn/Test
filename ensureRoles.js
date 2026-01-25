4147a4fb67a1abc60133cd1608ae477a606de554
zzzayyynnn <isislamanda@gmail.com>
2026-01-25 10:54:07 +0800

    Create ensureRoles.js

---
+++ ensureRoles.js
@@ -0,0 +1,31 @@
+const raids = [
+  "Subway",
+  "Infernal",
+  "Insect",
+  "Igris",
+  "Demon Castle",
+  "Elves",
+  "Goblin",
+];
+
+async function ensureRaidRoles(guild) {
+  const roles = {};
+
+  for (const raid of raids) {
+    let role = guild.roles.cache.find(r => r.name === raid);
+
+    if (!role) {
+      role = await guild.roles.create({
+        name: raid,
+        color: "DarkRed",
+        reason: "Raid self-role",
+      });
+    }
+
+    roles[raid] = role.id;
+  }
+
+  return roles;
+}
+
+module.exports = { ensureRaidRoles, raids };
